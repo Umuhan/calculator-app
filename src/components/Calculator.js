7e970bc10fa4b136d6861b8d82f6a9b1c959a9f0
Umu <hanny.ally123@gmail.com>
2023-09-20 13:10:26 +0300

    Add Calculator.js file

---
+++ src/components/Calculator.js
@@ -0,0 +1,33 @@
+const Calculator = () => (
+  <main className="">
+    <section className="calculator">
+      <header className="header">
+        <div className="calc">0</div>
+      </header>
+
+      <button className="btn" type="button">
+        Ac
+      </button>
+      <button type="button">DEL</button>
+      <button type="button">/</button>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button">*</button>
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button">+</button>
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button">-</button>
+      <button type="button">.</button>
+      <button type="button">0</button>
+      <button className="btn" type="button">
+        =
+      </button>
+    </section>
+  </main>
+);
+export default Calculator;
