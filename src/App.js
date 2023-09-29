4faa6d152ada64bfadd70cf4786baf8ba0c3d955
Umu <hanny.ally123@gmail.com>
2023-09-29 11:42:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 
-const App = () => (
+export default function App() {
   <div>
     <Calculator />
-  </div>
-);
-
-export default App;
+  </div>;
+}
