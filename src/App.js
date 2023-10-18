201016b9c79f20efb183d12d21fb3b9531f252c2
Umu <hanny.ally123@gmail.com>
2023-10-19 02:42:48 +0300

    Add QuoteDisplay component to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import Calculator from './components/Calculator';
+import React from "react";
+import Calculator from "./components/Calculator";
+import { QuoteDisplay } from "./components/QuoteDisplay";
 
 export default function App() {
   return (
     <div>
       <Calculator />
+      <QuoteDisplay />
     </div>
   );
 }
