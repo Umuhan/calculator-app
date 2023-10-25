84862f38516ee77ab07dd2a317b33120967fa882
Umu <hanny.ally123@gmail.com>
2023-10-25 15:51:18 +0300

    Update APP.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
 import QuoteDisplay from './components/QuoteDisplay';
+import Home from './components/Home';
 
-export default function App() {
-  return (
+const App = () => (
+  <Router>
     <div>
-      <Calculator />
-      <QuoteDisplay />
+      <nav>
+        <ul className="pages">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quote">QuoteDisplay</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/quote" element={<QuoteDisplay />} />
+      </Routes>
     </div>
-  );
-}
+  </Router>
+);
+
+export default App;
